9d5c8e7d8f59ca17e4f71576639c042f289226db
wpweaver <wpweaver@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-09-04 17:38:30 +0000

    Fixed incomplete fix
    git-svn-id: https://plugins.svn.wordpress.org/show-posts/trunk@1238214 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- atw-show-posts.php
+++ atw-show-posts.php
@@ -5,7 +5,7 @@ Plugin URI: http://WeaverTheme.com
 Description: Weaver Show Posts - Show  posts or custom posts within your Theme's pages or posts using a shortcode and a form-based interface.
 Author: wpweaver
 Author URI: http://weavertheme.com/about/
-Version: 1.3.2
+Version: 1.3.3
 
 License: GPL
 
@@ -30,7 +30,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* CORE FUNCTIONS
 */
 
-define ( 'WEAVER_SHOWPOSTS_VERSION','1.3.2');
+define ( 'WEAVER_SHOWPOSTS_VERSION','1.3.3');
 define ( 'WEAVER_SHOWPOSTS_MINIFY','.min');		// '' for dev, '.min' for production
 define ( 'WEAVER_SHOWPOSTS_TEMPLATE', false);      // future feature
 
